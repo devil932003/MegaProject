36449165bac9336947a8da879f5fe37f37d527f9
devil932003 <142711349+devil932003@users.noreply.github.com>
2025-04-19 23:22:59 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -2,9 +2,21 @@ const dotenv = require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
 const app = express();
+const connectToDb = require('./db/db.js');
+const cookieParser = require('cookie-parser');
 
+const userRoutes = require('./routes/user.routes');
+
+connectToDb();
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+app.use('/users', userRoutes);
+
 module.exports = app;
